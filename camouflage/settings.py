e995e44ef3710089273b0bf1918d67a4177e2dbc
whyme <77373533+whyme-duh@users.noreply.github.com>
2023-12-19 10:36:14 +0545

    Update settings.py

--- camouflage/settings.py
+++ camouflage/settings.py
@@ -78,18 +78,11 @@ WSGI_APPLICATION = 'camouflage.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':os.getenv('DB_NAME') ,
-        'USER': 'postgres',
-        'PASSWORD': 'GkkssR@123',
-        'HOST': 'localhost',
-        'PORT': os.getenv('DB_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
 }
 
 
