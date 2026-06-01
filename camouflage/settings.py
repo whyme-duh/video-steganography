04ab0704822689b52e283449304e99936be0b011
whyme <ritikshrestha94@gmail.com>
2026-06-01 20:01:35 +0545

    almost complete the design issues

--- camouflage/settings.py
+++ camouflage/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*' ,'192.168.1.153:3000','camouflagenepa.xyz']
 
